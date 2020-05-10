f115d5c5bd67ee5282bffe332c188cc335d052e6
satoshi <angelsb94@gmail.com>
2020-05-10 14:00:16 -0500

    iniciando proyecto de express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const { config } = require('./config/index');
+
+app.get('/', function(req, res) {
+  res.send("Hello word");
+})
+
+app.get('/json', function(req,res) {
+  res.json({hello: 'word'})
+});
+
+app.listen(config.port, function() {
+  console.log(`Escuchando en http://localhost:${config.port}`);
+});
