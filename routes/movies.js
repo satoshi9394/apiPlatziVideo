10cfd316e797caa1ca80920a22847b05e7e5f314
satoshi <angelsb94@gmail.com>
2020-05-26 17:49:46 -0500

    finalizando el proyecto

--- routes/movies.js
+++ routes/movies.js
@@ -8,8 +8,8 @@ const {
 } = require('../utils/schema/movies')
 
 const validationHandler = require('../utils/middleware/validationHandler')
-
-
+const cacheResponse = require('../utils/cacheResponse')
+const { FIVE_MINUTES_IN_SECONDS, SIXTY_MINUTES_IN_SECONDS } = require('../utils/time')
 
 function moviesApi(app) {
   const router = express.Router();
@@ -17,6 +17,8 @@ function moviesApi(app) {
   const moviesService = new MoviesService();
 
   router.get("/", async function(req, res, next){
+    cacheResponse(res, FIVE_MINUTES_IN_SECONDS)
+
     const { tags } = req.query;
     try {
       const movies = await moviesService.getMovies({ tags });
@@ -32,6 +34,7 @@ function moviesApi(app) {
 
   router.get("/:movieId",validationHandler({ movieId: movieIdSchema }, 'params'), 
     async function(req, res, next){
+    cacheResponse(res, SIXTY_MINUTES_IN_SECONDS)  
     const { movieId } = req.params
     try {
       const movies = await moviesService.getMovie({ movieId });
