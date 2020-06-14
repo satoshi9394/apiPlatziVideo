ac902d3597c471b07f2737d5a93de92ac04d343a
satoshi <angelsb94@gmail.com>
2020-06-14 18:56:17 -0500

    agregando peliculas y usuarios a traves de scripts a demas de utilizar la estrategia basica de passport

--- config/index.js
+++ config/index.js
@@ -7,7 +7,11 @@ const config = {
   db_user:      process.env.DB_USER,
   db_password:  process.env.DB_PASSWORD,
   db_host:      process.env.DB_HOST,
-  db_name:      process.env.DB_NAME
+  db_name:      process.env.DB_NAME,
+  defaultAdminPassword: process.env.DEFAULT_ADMIN_PASSWORD,
+  defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
+  publicApiKeyToken: process.env.PUBLIC_API_KEY_TOKEN,
+  adminApiKeyToken: process.env.ADMIN_API_KEY_TOKEN
 }
 
 module.exports = { config }
